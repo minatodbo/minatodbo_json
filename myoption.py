f1d554c084982297efdb590336d19472d8040212
minatodbo <minatodbo@gmail.com>
2024-11-21 13:20:55 -0500

    Create myoption.py

---
+++ myoption.py
@@ -0,0 +1,12 @@
+Identified Box Spreads:
+    Client Ticker     Maturity  Buy Call Strike  Sell Call Strike  \
+0  ClientA    ABC  2024-12-31               95               105   
+1  ClientA    ABC  2024-12-31               95               105   
+
+   Sell Put Strike  Buy Put Strike  Underlying Price  Box Quantity  \
+0               95             105               100             5   
+1               95             105               100            10   
+
+      Spread Type  
+0  Long Box Spread  
+1  Short Box Spread  
